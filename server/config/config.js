67cb2d5d3f5e4876d86b79fcbba1cc5f7046150e
keith <skchiah@gmail.com>
2018-05-29 15:19:50 +0800

    First commit

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
+
+console.log("Enviroment: ", env);
+console.log("Database: ", process.env.MONGODB_URI);
